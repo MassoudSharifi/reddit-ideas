bc7d8e5c16b2ae2b379a6a3a0dbb67612abc39e2
Doha Faridi <dohafaridi@gmail.com>
2019-12-17 22:43:41 +0100

    Handling exceptions

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,10 +4,18 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { IdeaModule } from './idea/idea.module';
+import { APP_FILTER } from '@nestjs/core';
+import { HttpExceptionFilter } from './utils/http-exception.filter';
 
 @Module({
   imports: [TypeOrmModule.forRoot(), IdeaModule],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [
+    AppService,
+    {
+      provide: APP_FILTER,
+      useClass: HttpExceptionFilter,
+    },
+  ],
 })
 export class AppModule {}
