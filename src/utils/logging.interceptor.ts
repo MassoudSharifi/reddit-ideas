1c1c6397fc4b3a2a09301a8dd3d597acdec22474
Doha Faridi <dohafaridi@gmail.com>
2019-12-17 23:09:19 +0100

    Log user interactions

---
+++ src/utils/logging.interceptor.ts
@@ -0,0 +1,30 @@
+import {
+  NestInterceptor,
+  ExecutionContext,
+  CallHandler,
+  Logger,
+} from '@nestjs/common';
+import { Observable } from 'rxjs';
+import { tap } from 'rxjs/operators';
+
+export class LoggingInterceptor implements NestInterceptor {
+  intercept(context: ExecutionContext, next: CallHandler): Observable<any> {
+    const now = Date.now();
+    const request = context.switchToHttp().getRequest();
+    if (request) {
+      const method = request.method;
+      const url = request.url;
+
+      return next
+        .handle()
+        .pipe(
+          tap(() =>
+            Logger.log(
+              `${method} ${url} ${Date.now() - now}ms`,
+              context.getClass().name,
+            ),
+          ),
+        );
+    }
+  }
+}
